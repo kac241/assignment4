30abd6c4ded64068123b258e261e06f3b687f364
kac241 <kac241@pitt.edu>
2017-04-12 22:35:55 -0400

    Completed

--- assignment4.js
+++ assignment4.js
@@ -1,17 +1,18 @@
-// The anonymous function below will fire on page load
-
-// Some things to consider
-// $.ajax(); to make your requests a little easier. Or the vanilla js way, it's up to you.
-// $.on(); for event handling
-// Remember, selecting elements in jQuery is like selecting them in CSS
-// You'll probably have to manipulate some strings
-// some jQuery functions to help display results
-// $.show(), $.hide(), $.slideup(), $.slidedown(), $.fadein(), $.fadeout()
-// Add content from requests with something like
-// $.html(), $.text(), etc.
-// keyup events could be helpful to get value of field as the user types
 
 (function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
+	$.ajax({ type:'GET', dataType:'json', url:"http://www.mattbowytz.com/simple_api.json?data=all", success:function(searchData){
+		$(".flexsearch-input").keyup(function(){
+			var searchTerm = $(".flexsearch-input").val();				
+			$(".autocomplete").empty();
+			$.each(searchData.data, function(i1,v1){
+				$.each(v1, function(i2,v2){	
+					if(searchTerm.toLowerCase() == v2.substring(0,searchTerm.length).toLowerCase()){
+						if(searchTerm.length > 0){
+							$('<div class="autocomplete"><br>'+ v2 +'<br></div>').appendTo("#mainForm");
+						}	
+					}	
+				});
+			});
+		});	
+	}});
 })();
